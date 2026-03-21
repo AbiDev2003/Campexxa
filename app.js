a66911ea231ccdbf02b01f761a6c6c8b089edb2f
AbiDev2003 <2003abinashdash@gmail.com>
2026-03-21 20:22:26 +0530

    fix: bind server to 0.0.0.0 for render

--- app.js
+++ app.js
@@ -177,6 +177,6 @@ app.use((err, req, res, next) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Serving on port ${PORT} !`)
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Serving on port ${PORT}`);
+});
