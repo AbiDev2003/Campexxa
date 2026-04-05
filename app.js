29fa985dff57121f2512e6ba7d7d2a5a92485f55
AbiDev2003 <2003abinashdash@gmail.com>
2026-04-05 23:21:48 +0530

    artilery logs capture

--- app.js
+++ app.js
@@ -182,9 +182,18 @@ app.all(/(.*)/, (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
+    // const { statusCode = 500 } = err;
+    // if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    // res.status(statusCode).render('error', { err })
+
+    console.error("🔥 ERROR START ==========");
+    console.error(err.stack || err);
+    console.error("🔥 ERROR END ==========");
+
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
+
+    res.status(statusCode).render('error', { err });
 })
 
 const PORT = process.env.PORT || 3000
