b4af2a565a4d6fc5e39af0bc5613e884e0166978
AbiDev2003 <2003abinashdash@gmail.com>
2026-04-05 23:35:21 +0530

    artilllery test fix

--- utils/sendPaginatedResponse.js
+++ utils/sendPaginatedResponse.js
@@ -1,7 +1,8 @@
 const renderPartial = require('./renderPartial');
 
 module.exports.sendPaginatedResponse = async ({ req, res, view, dataKey, data, hasMore }) => {
-  if (req.xhr || req.headers.accept.includes('application/json')) {
+  const accept = req.headers.accept || "";
+  if (req.xhr || accept.includes('application/json')) {
     const html = await renderPartial(req, res, view, { [dataKey]: data });
     return res.json({ html, hasMore });
   }
